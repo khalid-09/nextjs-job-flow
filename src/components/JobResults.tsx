408c95ac68051e5da5ae8fa851cc1422d883a704
Khalid Ansari <khaliljpc@gmail.com>
2024-05-11 11:56:04 +0530

    feat: add pagination

--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -3,14 +3,20 @@ import JobListItem from "./JobListItem";
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 interface JobResultsProps {
   filterValues: JobFilterValues;
+  page?: number;
 }
 
-const JobResults = async ({
-  filterValues: { q, type, location, remote },
-}: JobResultsProps) => {
+const JobResults = async ({ filterValues, page = 1 }: JobResultsProps) => {
+  const { q, type, location, remote } = filterValues;
+
+  const jobsPerPage = 6;
+  const skip = (page - 1) * jobsPerPage;
+
   const searchString = q
     ?.split(" ")
     .filter((word) => word.length > 0)
@@ -38,10 +44,16 @@ const JobResults = async ({
     ],
   };
 
-  const jobs = await prisma.job.findMany({
+  const jobsPromise = await prisma.job.findMany({
     where,
     orderBy: { createdAt: "desc" },
+    take: jobsPerPage,
+    skip,
   });
+
+  const countPromise = prisma.job.count({ where });
+
+  const [jobs, totalResults] = await Promise.all([jobsPromise, countPromise]);
 
   return (
     <div className="grow space-y-4">
@@ -55,8 +67,67 @@ const JobResults = async ({
           No jobs found. Try adjusting your search filters.
         </p>
       )}
+      {jobs.length > 0 && (
+        <Pagination
+          filterValues={filterValues}
+          currentPage={page}
+          totalPages={Math.ceil(totalResults / jobsPerPage)}
+        />
+      )}
     </div>
   );
 };
 
 export default JobResults;
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  filterValues: JobFilterValues;
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  filterValues: { q, type, location, remote },
+}: PaginationProps) => {
+  const generatePageLink = (page: number) => {
+    const searchParams = new URLSearchParams({
+      ...(q && { q }),
+      ...(type && { type }),
+      ...(location && { location }),
+      ...(remote && { remote: "true" }),
+      page: page.toString(),
+    });
+
+    return `/?${searchParams.toString()}`;
+  };
+
+  return (
+    <div className="flex items-center justify-between">
+      <Link
+        href={generatePageLink(currentPage - 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage <= 1 && "invisible",
+        )}
+      >
+        <ArrowLeft size={16} />
+        Previous Page
+      </Link>
+      <span className="font-semibold">
+        Page {currentPage} of {totalPages}
+      </span>
+      <Link
+        href={generatePageLink(currentPage + 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage >= totalPages && "invisible",
+        )}
+      >
+        Next Page
+        <ArrowRight size={16} />
+      </Link>
+    </div>
+  );
+};
